122224f5d828dbdc8de461bc663cd214313d57a7
Nihal Shaikh <mohammednihal665@gmail.com>
2026-09-03 00:55:16 +0530

    chore: establish Flask project structure

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,10 +6,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "FinSight AI is running!"
+    return render_template("home.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-    
